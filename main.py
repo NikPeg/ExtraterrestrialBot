370a6854cb90c98044edaa69bec5d4f2562dc956
Никита Пеганов <peganov.nik@gmail.com>
2021-08-09 00:39:39 +0300

    Add admin keyboard

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import requests
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
 import random
+
+import vk_api
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 vk_session = vk_api.VkApi(token='41f09080cb1af69a2d297b4452558b4c40615070d2c3faca34881fc3d0da57466abf92fc1edbdf8cc1826')
 longpoll = VkLongPoll(vk_session)
@@ -13,14 +13,23 @@ base_keyboard.add_button('Расходы', color=VkKeyboardColor.SECONDARY)
 base_keyboard.add_button('Казна', color=VkKeyboardColor.SECONDARY)
 base_keyboard.add_line()
 base_keyboard.add_button('Счастье', color=VkKeyboardColor.SECONDARY)
-base_keyboard.add_button('Миссии', color=VkKeyboardColor.SECONDARY)
+base_keyboard.add_button('Желания', color=VkKeyboardColor.SECONDARY)
 base_keyboard.add_button('Досье', color=VkKeyboardColor.SECONDARY)
 base_keyboard.add_line()
 base_keyboard.add_button('Вызвать секретаря', color=VkKeyboardColor.PRIMARY)
+admin_keyboard = VkKeyboard(one_time=True)
+admin_keyboard.add_button('Формат запроса', color=VkKeyboardColor.SECONDARY)
+admin_keyboard.add_button('Досье', color=VkKeyboardColor.SECONDARY)
+admin_keyboard.add_line()
+admin_keyboard.add_button('Стоп', color=VkKeyboardColor.NEGATIVE)
+admin_keyboard.add_button('Вердить', color=VkKeyboardColor.POSITIVE)
+admin_keyboard.add_button('Пропустить', color=VkKeyboardColor.PRIMARY)
 time_of_day = ['Доброе утро', 'Добрый день', 'Добрый вечер']
 secretary_phrases = ['Звали?', 'Чем могу быть полезна?', 'Чем могу помочь?', 'Что-то случилось?',
                      'Извините, замешкалась. Чем могу помочь?',
                      'Кофейку?', 'Чем могу служить?', 'Что приключилось на этот раз?']
+
+ADMIN_ID = 184272849
 
 
 def send_income(user_id):
@@ -90,13 +99,13 @@ def send_wishes(user_id):
     )
 
 
-def send_dossier(user_id):
+def send_dossier(user_id=ADMIN_ID):
     vk.messages.send(
         user_id=event.user_id,
         message='Вы открыли папку с досье на каждого персонажа, кого Вы встречали или видели. '
                 'Введите имя или прозвище того, чьё досье хотите прочитать:',
         random_id=random.randint(0, 1000000),
-        keyboard=base_keyboard.get_keyboard(),
+        keyboard=(base_keyboard if user_id != ADMIN_ID else admin_keyboard).get_keyboard(),
     )
 
 
@@ -104,27 +113,98 @@ def call_secretary(user_id):
     vk.messages.send(
         user_id=event.user_id,
         message=f'Секретарь:\n{random.choice(time_of_day)}, правитель! {random.choice(secretary_phrases)}',
-        # добавить правильное обращение. Направляющая и прочее
         random_id=random.randint(0, 1000000),
         keyboard=base_keyboard.get_keyboard(),
     )
 
 
-actions = {'Доходы': send_income, 'Расходы': send_expenses, 'Казна': send_treasury, 'Счастье': send_happiness,
-           'Миссии': send_wishes, 'Досье': send_dossier, 'Вызвать секретаря': call_secretary}
+def send_format():
+    vk.messages.send(
+        user_id=ADMIN_ID,
+        message='Формат запроса:\n\n'
+                'Страна величина — возвращает значение величины\n'
+                'Страна величина +-значение — увеличивает/уменьшает величину на значение\n'
+                'Страна величина +-число% — увеличивает/уменьшает величину на число процентов',
+        random_id=random.randint(0, 1000000),
+        keyboard=admin_keyboard.get_keyboard(),
+    )
+
+
+def send_next():
+    # не забудь учесть удаление сообщений игроками
+    vk.messages.send(
+        user_id=ADMIN_ID,
+        message='Щаща',
+        random_id=random.randint(0, 1000000),
+        keyboard=admin_keyboard.get_keyboard(),
+    )
+
+
+def stop_verding():
+    vk.messages.send(
+        user_id=ADMIN_ID,
+        message='Режим выдачи вердов остановлен.',
+        random_id=random.randint(0, 1000000),
+        keyboard=admin_keyboard.get_keyboard(),
+    )
+
+
+def start_verding():
+    vk.messages.send(
+        user_id=ADMIN_ID,
+        message='Режим выдачи вердов включен.',
+        # добавить правильное обращение. Направляющая и прочее
+        random_id=random.randint(0, 1000000),
+        keyboard=admin_keyboard.get_keyboard(),
+    )
+
+
+user_actions = {'Доходы': send_income, 'Расходы': send_expenses, 'Казна': send_treasury, 'Счастье': send_happiness,
+                'Желания': send_wishes, 'Досье': send_dossier, 'Вызвать секретаря': call_secretary}
+admin_actions = {'Формат запроса': send_format, 'Досье': send_dossier, 'Пропустить': send_next, 'Стоп': stop_verding,
+                 'Вердить': start_verding}
+
+
+def user_handler(event):
+    if event.text in user_actions:
+        user_actions[event.text](event.user_id)
+    elif event.text == "Начать":
+        vk.messages.send(
+            user_id=event.user_id,
+            message='Добро пожаловать в Extraterrestrial:Reload! Интересной и приятной игры!',
+            random_id=random.randint(0, 1000000),
+            keyboard=base_keyboard.get_keyboard(),
+        )
+    # elif персонаж
+    else:
+        vk.messages.send(
+            user_id=ADMIN_ID,
+            message='Тут будет название страны или персонажа',
+            random_id=random.randint(0, 1000000),
+            keyboard=base_keyboard.get_keyboard(),
+            forward_messages=event.message_id,
+        )
+
+
+def admin_handler(event):
+    if event.text in admin_actions:
+        admin_actions[event.text]()
+    elif event.text == "Начать":
+        vk.messages.send(
+            user_id=event.user_id,
+            message='Добро пожаловать в Extraterrestrial:Reload! Интересной и приятной игры!',
+            random_id=random.randint(0, 1000000),
+            keyboard=admin_keyboard.get_keyboard(),
+        )
+
+
 while True:
     try:
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.from_user:
-                if event.text in actions:
-                    actions[event.text](event.user_id)
-                elif event.text == "Начать":
-                    vk.messages.send(
-                        user_id=event.user_id,
-                        message='Добро пожаловать в Extraterrestrial:Reload! Интересной и приятной игры!',
-                        random_id=random.randint(0, 1000000),
-                        keyboard=base_keyboard.get_keyboard(),
-                    )
-                # elif персонаж
+                if event.user_id == ADMIN_ID:
+                    admin_handler(event)
+                else:
+                    user_handler(event)
     except Exception as e:
         print(e)
